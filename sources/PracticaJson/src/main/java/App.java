25f8d02801f969ebefc8c3421de8e701bc696574
kellytbn <kellytbn14@gmail.com>
2021-03-23 22:06:57 -0500

    Se crea método para leer archivo json y mapear en objeto User

--- sources/PracticaJson/src/main/java/App.java
+++ sources/PracticaJson/src/main/java/App.java
@@ -1,14 +1,35 @@
 import com.google.gson.Gson;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 
 public class App {
 
-    private static Gson gson = new Gson();
-    private File fileUser;
-    private static final String jsonFilePath = "src/main/resource/User.json";
+    private static final Gson gson = new Gson();
+    private static File fileUser;
+    private static final String jsonFilePath = "src/main/resources/User.json";
 
     public static void main(String[] args) {
 
+    }
+
+    static User jsonToUser(){
+        User user = new User();
+        try{
+            fileUser = new File(jsonFilePath);
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileUser));
+            String line = "";
+            String json = "";
+            while ((line = bufferedReader.readLine())!=null){
+                json = json + line;
+            }
+            bufferedReader.close();
+
+            user = gson.fromJson(json, User.class);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return user;
     }
 }
