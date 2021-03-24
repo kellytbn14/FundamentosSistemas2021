c770ee719675da17a3e7e9a9a1e6f00cc4f63edc
kellytbn <kellytbn14@gmail.com>
2021-03-23 21:29:05 -0500

    Se crean los objetos para mapear el json

---
+++ sources/PracticaJson/src/main/java/User.java
@@ -0,0 +1,39 @@
+public class User {
+
+    private String firstName;
+    private String lastName;
+    private Integer age;
+    private Address address;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+}
