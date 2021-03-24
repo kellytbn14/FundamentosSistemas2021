f73d92f2620febd8170c10035adfd136f29e0180
kellytbn <kellytbn14@gmail.com>
2021-03-23 21:37:26 -0500

    Se crea el método main y se instancian las librerías Gson y File

---
+++ sources/PracticaJson/src/main/java/App.java
@@ -0,0 +1,14 @@
+import com.google.gson.Gson;
+
+import java.io.File;
+
+public class App {
+
+    private static Gson gson = new Gson();
+    private File fileUser;
+    private static final String jsonFilePath = "src/main/resource/User.json";
+
+    public static void main(String[] args) {
+
+    }
+}
