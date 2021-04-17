49f02e768c4d05c87056a5f23c814e91474e8928
kellytbn <kellytbn14@gmail.com>
2021-04-17 15:08:41 -0500

    Se agrega la lista de vendedores. Se propone nueva estructura

--- sources/Taller Json/Ejercicio 3/src/main/java/JsonPrueba.java
+++ sources/Taller Json/Ejercicio 3/src/main/java/JsonPrueba.java
@@ -5,39 +5,55 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class JsonPrueba {
+
     public static void main(String[] args) {
         Vendedor vendedor1 = new Vendedor(26, "Kelly", "Tobón");
-        Vendedor vendedor2 = new Vendedor(25, "Stefania", "Montoya");
-        Vendedor vendedor3 = new Vendedor(21, "Alan", "Perez");
-
         Cliente cliente1 = new Cliente("Sergio Tobón", "Calle 13", "3128501902");
         Cliente cliente2 = new Cliente("Monica Montoya", "Carrera 15", "3108319987");
         Cliente cliente3 = new Cliente("William Tobón", "Calle 48", "3207742404");
 
-        List<Cliente> clientes = new ArrayList<Cliente>();
-        clientes.add(cliente1);
-        clientes.add(cliente2);
-        clientes.add(cliente3);
+        List<Cliente> clientesV1 = new ArrayList<Cliente>();
+        clientesV1.add(cliente1);
+        clientesV1.add(cliente2);
+        clientesV1.add(cliente3);
+        vendedor1.setClientes(clientesV1);
 
-        JSONObject jsonVendedor1 = new JSONObject(vendedor1);
-        JSONArray jsonClientes = new JSONArray();
+        Vendedor vendedor2 = new Vendedor(25, "Stefania", "Montoya");
+        Cliente cliente4 = new Cliente("Luisa Cardona", "Calle 32", "333555666");
+        Cliente cliente5 = new Cliente("Carlos Lopez", "Carrera 232", "447778888");
+        Cliente cliente6 = new Cliente("Luis Gomez", "Calle 2323", "778889999");
 
-        try{
-            for (Cliente cliente : clientes){
-                JSONObject jsonCliente = new JSONObject();
-                jsonCliente.put("nombre", cliente.getNombre());
-                jsonCliente.put("direccion", cliente.getDireccion());
-                jsonCliente.put("telefono", cliente.getTelefono());
-                jsonClientes.put(jsonCliente);
-            }
-            JSONObject jsonVendedorClientes = new JSONObject();
-            jsonVendedorClientes.put("vendedor", jsonVendedor1);
-            jsonVendedorClientes.put("clientes", jsonClientes);
+        List<Cliente> clientesV2 = new ArrayList<Cliente>();
+        clientesV2.add(cliente4);
+        clientesV2.add(cliente5);
+        clientesV2.add(cliente6);
+        vendedor2.setClientes(clientesV2);
+
+        Vendedor vendedor3 = new Vendedor(21, "Alan", "Perez");
+        Cliente cliente7 = new Cliente("Mercela Montoya", "Calle 2323", "745745545");
+        Cliente cliente8 = new Cliente("Esteban Montoya", "Carrera e23", "785785785");
+
+        List<Cliente> clientesV3 = new ArrayList<Cliente>();
+        clientesV3.add(cliente7);
+        clientesV3.add(cliente8);
+        vendedor3.setClientes(clientesV3);
+
+        List<Vendedor> vendedores = new ArrayList<>();
+        vendedores.add(vendedor1);
+        vendedores.add(vendedor2);
+        vendedores.add(vendedor3);
+
+        parseToJson(vendedores);
+
+    }
+
+    public static void parseToJson(List<Vendedor> vendedores) {
+        try {
+            JSONArray jsonVendedorClientes = new JSONArray(vendedores);
 
             System.out.println(jsonVendedorClientes);
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 }
